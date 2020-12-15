352459d90b9c7943781396fd545eb68fb5970b15
yunweneric <yunweneric@gmail.com>
2020-12-15 17:53:31 +0000

    modified api

--- src/app/youtubeapi.service.ts
+++ src/app/youtubeapi.service.ts
@@ -5,7 +5,7 @@ import { HttpClient } from '@angular/common/http'
   providedIn: 'root'
 })
 export class YoutubeapiService {
-  url = 'https://www.googleapis.com/youtube/v3/search?path=snippet&key=AIzaSyCpZ_WOL_e2ro5cl7uLMYLcY9K7q8NivxY&type=video&q=christmas%20carol&maxResults=100'
+  url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCpZ_WOL_e2ro5cl7uLMYLcY9K7q8NivxY&type=video&q=christmas%20carol&maxResults=10&videoduration=any'
 
   constructor(private http: HttpClient) { }
   getdata() {
