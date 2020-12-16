062838804a7f5c97b3aa7d57e085142017a90f99
yunweneric <yunweneric@gmail.com>
2020-12-16 11:26:15 +0100

    modified api

--- src/app/youtubeapi.service.ts
+++ src/app/youtubeapi.service.ts
@@ -10,7 +10,7 @@ import {SearchInterface} from './search-interface'
 })
 export class YoutubeapiService {
   private API_URL = 'https://www.googleapis.com/youtube/v3/search';
-  private API_TOKEN = 'AIzaSyCpZ_WOL_e2ro5cl7uLMYLcY9K7q8NivxY';
+  private API_TOKEN = 'AIzaSyDov_AhJmCFgGaeInPBrSBq4zjoUd_RmFM';
   url = 'https://www.googleapis.com/youtube/v3/search?part=snippet&key=AIzaSyCpZ_WOL_e2ro5cl7uLMYLcY9K7q8NivxY&type=video&q=christmas%20carol&maxResults=10&videoduration=any'
 
   constructor(private http: HttpClient) { }
@@ -18,7 +18,7 @@ export class YoutubeapiService {
     return this.http.get(this.url)
   }
   getVideos(query: string): Observable <any> {
-    const url = `${this.API_URL}?q=${query}&key=${this.API_TOKEN}&part=snippet&type=video&maxResults=10`;
+    const url = `${this.API_URL}?q=${query}&key=${this.API_TOKEN}&part=snippet&type=video&maxResults=6`;
     return this.http.get(url)
       .pipe(
         map((response: any) => response.items)
--- src/app/youtubeonly/youtubeonly.component.ts
+++ src/app/youtubeonly/youtubeonly.component.ts
@@ -1,21 +1,39 @@
 import { Component, OnInit } from '@angular/core';
-import { YoutubeapiService } from '../youtubeapi.service'
+import { YoutubeapiService } from '../youtubeapi.service';
+import { SearchInterface } from '../search-interface';
 
 @Component({
   selector: 'app-youtubeonly',
   templateUrl: './youtubeonly.component.html',
-  styleUrls: ['./youtubeonly.component.css']
+  styleUrls: ['./youtubeonly.component.css'],
 })
 export class YoutubeonlyComponent implements OnInit {
-  collection: any
+  collection: any;
+  videos: any;
+  url1: any
+  url2: any
 
-  constructor(private video: YoutubeapiService) { }
+  constructor(private video: YoutubeapiService) {}
 
   ngOnInit(): void {
-    this.video.getdata().subscribe(data => {
-      this.collection = data
-      console.log(data);
-    })
-  }
+    this.video.getVideos('christmas songs').subscribe(
+      // lista => {
+      //   for (let element of lista["items"]) {
+      //   this.videos.push(element)
+      //   }
 
+      (data) => {
+        // for (let element of data) {
+        //   this.videos.push(element);
+        //   this.collection = data;
+        //   console.log(this.videos)
+        // }
+        this.collection = data;
+        console.log(this.collection.items);
+        // console.log(this.videos);
+        this.url1 = "https://www.youtube.com/embed"
+
+      }
+    );
+  }
 }
