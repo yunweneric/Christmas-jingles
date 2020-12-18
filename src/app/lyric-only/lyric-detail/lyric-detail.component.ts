2179454726b76f0770e7c9e51bff1ff907d82764
yunweneric <yunweneric@gmail.com>
2020-12-18 18:28:18 +0100

    updated lyric UI

--- src/app/lyric-only/lyric-detail/lyric-detail.component.ts
+++ src/app/lyric-only/lyric-detail/lyric-detail.component.ts
@@ -10,6 +10,8 @@ import {LyricServiceService} from 'src/app/lyric-service.service'
 export class LyricDetailComponent implements OnInit {
   // @Input() data;
   collection: any;
+  title: any;
+  author: any
 
   constructor(private router:ActivatedRoute, private http: LyricServiceService ) { }
 
@@ -17,6 +19,8 @@ export class LyricDetailComponent implements OnInit {
     console.log(this.router.snapshot.params.id);
     this.http.getcurrentlyric(this.router.snapshot.params.id).subscribe(data =>{
       this.collection = data
+      this.title = this.collection.title
+      this.author = this.collection.author
       console.log(this.collection.lyric);
 
       
